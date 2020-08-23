c360f8734f2e2f8fd2fba9853a69151fdbf3b897
GihunLee <opcrisis@kaist.ac.kr>
2020-08-23 21:38:36 +0900

    baseline exp

--- exp.sh
+++ exp.sh
@@ -10,7 +10,7 @@ gpu=0
 exp_name="moco_split_nosmall"
 
 # pretrain encoder
-python ./main.py -a resnet18 --builder moco --batch-size 128 --lr 0.015 --epochs 200 --schedule 120 160 --gpu $gpu --dataset tiny-imagenet --data-path $data_path --mlp --moco-t 0.2 --aug-plus --cos --exp-name $exp_name
+python ./main.py -a resnet18 --builder moco --batch-size 128 --lr 0.015 --epochs 100 --schedule 60 80 --gpu $gpu --dataset tiny-imagenet --data-path $data_path --mlp --moco-t 0.2 --aug-plus --cos --exp-name $exp_name
 
 # linear classification protocol
 python ./lincls.py -a resnet18 --lr 3.0 --epochs 100 --dataset tiny-imagenet --data-path $data_path --pretrained ./results/pretrained/${exp_name}.pth.tar --gpu $gpu --exp-name $exp_name
