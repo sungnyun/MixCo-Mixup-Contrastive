1cf155d7752ad6d6f31a3fb91459011db024c34c
sungnyun <ksn4397@gmail.com>
2020-09-04 03:11:14 +0900

    save the last file

--- lincls.py
+++ lincls.py
@@ -316,19 +316,19 @@ def main_worker(gpu, ngpus_per_node, args):
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
 
-        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
-                and args.rank % ngpus_per_node == 0):
+    # always saves at the end of training    
+    else:
+        if not args.multiprocessing_distributed or (args.multiprocessing_distributed and args.rank % ngpus_per_node == 0):
             save_checkpoint({
                 'epoch': epoch + 1,
                 'arch': args.arch,
                 'state_dict': model.state_dict(),
                 'best_acc1': best_acc1,
                 'optimizer' : optimizer.state_dict(),
-            }, is_best)
+            }, is_best=False, filename='{}.pth.tar'.format(args.exp_name))
             if epoch == args.start_epoch:
                 sanity_check(model.state_dict(), args.pretrained)
                 
-    else:
         update_json(args.exp_name, 'lincls', [best_acc1.item(), acc5.item()])
 
 
@@ -431,7 +431,7 @@ def save_checkpoint(state, is_best, filename='test'):
     filename = os.path.join('./results/lincls', filename)
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+        shutil.copyfile(filename, './results/lincls/model_best.pth.tar')
 
 
 def sanity_check(state_dict, pretrained_weights):
