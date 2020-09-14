cfd0899efa891d4cdd2ce37980c14b24c2aa5556
sungnyun <ksn4397@gmail.com>
2020-09-14 08:54:18 +0000

    cifar experiments

--- experiments/exp_moco_res50.sh
+++ experiments/exp_moco_res50.sh
@@ -12,4 +12,4 @@ exp_name="moco_resnet50"
 python main_moco.py -a resnet50 --builder moco --batch-size 128 --lr 0.015 --epochs 200 --schedule 120 160 --dataset tiny-imagenet --data-path $data_path --mlp --moco-t 0.2 --aug-plus --cos --exp-name $exp_name --multiprocessing-distributed --dist-url tcp://localhost:10001
 
 # linear classification protocol
-#python lincls.py -a resnet50 --lr 3.0 --epochs 100 --schedule 60 80 --dataset tiny-imagenet --data-path $data_path --pretrained ./results/pretrained/${exp_name}.pth.tar --exp-name $exp_name --multiprocessing-distributed --dist-url tcp://localhost:10001
+python lincls.py -a resnet50 --lr 3.0 --epochs 100 --schedule 60 80 --dataset tiny-imagenet --data-path $data_path --pretrained ./results/pretrained/${exp_name}.pth.tar --exp-name $exp_name --multiprocessing-distributed --dist-url tcp://localhost:10001
