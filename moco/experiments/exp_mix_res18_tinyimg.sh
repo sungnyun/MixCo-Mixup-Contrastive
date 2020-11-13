865de18023625d916ae27ab237f07860de328cb7
sungnyun <ksn4397@gmail.com>
2020-11-13 12:15:37 +0000

    clean moco experiments

--- moco/experiments/exp_mix_res18_tinyimg.sh
+++ moco/experiments/exp_mix_res18_tinyimg.sh
@@ -5,8 +5,8 @@ mkdir -p ./results/pretrained
 mkdir -p ./results/lincls
 
 # set configs (let other configs unchanged if not needed)
-data_path="./data/tiny-imagenet-200"
-exp_name="exp_mix_res18_tinyimg"
+data_path="../dataset/tiny-imagenet-200"
+exp_name="exp_mixco_res18_tinyimg"
 
 # pretrain encoder
 python ./pretrain.py -a resnet18 --algo mixco --batch-size 128 --lr 0.015 --epochs 100 --cos --dataset tiny-imagenet --data-path $data_path --mlp --moco-t 0.2 --aug-plus --exp-name $exp_name --multiprocessing-distributed --dist-url tcp://localhost:10001 --gpu 0 1
